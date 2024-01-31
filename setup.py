defd4d959004efdb1b1ada1ae9fe74c33fc37952
Cristian Cretu <cristian.cretu2808@gmail.com>
2024-01-31 14:12:08 +0200

    added LICENSE

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="wal-asusctl",
+    version="0.1.0",
+    description="Control ASUS laptop LED modes using colors from wal.",
+    url="https://github.com/Flexksx/wal-asusrog"
+    author="Cretu Cristian",
+    author_email="",
+    license="MIT",
